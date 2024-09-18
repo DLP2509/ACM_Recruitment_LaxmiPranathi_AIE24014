4a2a0ad3ec272d3e0bada0e169faba08f160f695
Deekonda Laxmi Pranathi <laxmipranathideekonda@gmail.com>
2024-09-18 22:30:42 +0530

    Update and rename Q4.py to Q6.py

---
+++ Project Euler/Q6.py
@@ -0,0 +1,6 @@
+n = 100
+sum_of_squares = n * (n + 1) * (2 * n + 1) // 6
+sum_of_numbers = n * (n + 1) // 2
+square_of_sum = sum_of_numbers ** 2
+difference = square_of_sum - sum_of_squares
+print("The difference between the square of the sum and the sum of the squares is:", difference)
